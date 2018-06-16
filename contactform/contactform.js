fad66377e335f6644c467dce32aad14c2ea1ca54
Ayan Chowdhury <ayan.chowdhury1234@outlook.com>
2018-06-16 19:58:21 +0530

    trying contact form improve

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -89,26 +89,9 @@ jQuery(document).ready(function($) {
       }
     });
     if (ferror) return false;
-    else var str = $(this).serialize();
-    $.ajax({
-      type: "POST",
-      url: "contactform/contactform.php",
-      data: str,
-      success: function(msg) {
-        // alert(msg);
-        if (msg == 'OK') {
-          $("#sendmessage").addClass("show");
-          $("#errormessage").removeClass("show");
-          $('.contactForm').find("input, textarea").val("");
-        } else {
-          $("#sendmessage").removeClass("show");
-          $("#errormessage").addClass("show");
-          $('#errormessage').html(msg);
-        }
-
-      }
-    });
-    return false;
+   
+    return true;
   });
 
-});
+
+  });
